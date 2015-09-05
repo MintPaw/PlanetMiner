08677d2230c9c2291a947cf2b7d1ff4fcd19029b
MintPaw <jerusanders@gmail.com>
2015-09-04 21:45:37 -0600

    Generated map string

--- src/game/GameState.hx
+++ src/game/GameState.hx
@@ -2,9 +2,11 @@ package game;
 
 import flixel.FlxState;
 import flixel.FlxG;
+import flixel.tile.FlxTilemap;
 
 class GameState extends FlxState
 {
+	private var _tilemap:FlxTilemap;
 
 	public function new()
 	{
@@ -13,6 +15,24 @@ class GameState extends FlxState
 
 	public override function create():Void
 	{
+		{ // Setup tilemap
+			_tilemap = new FlxTilemap();
 
+			var startMap:String = "";
+			var rows:Int = 80;
+			var cols:Int = 45;
+			for (row in 0...rows) {
+				for (col in 0...cols) {
+					startMap += "0";
+					if (col != cols-1) startMap += ",";
+				}
+				if (row != rows-1) startMap += "\n";
+			}
+
+			trace(startMap);
+
+
+			add(_tilemap);
+		}
 	} 
 }
