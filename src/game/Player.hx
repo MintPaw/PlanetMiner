7116796aeb41396ef4ec16c148818de3b5fe4c65
MintPaw <jerusanders@gmail.com>
2015-09-05 13:23:46 -0600

    Added player graphic

--- src/game/Player.hx
+++ src/game/Player.hx
@@ -1,7 +1,9 @@
 package game;
 
 import flixel.FlxSprite;
+import flixel.FlxObject;
 import flixel.FlxG;
+import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.input.gamepad.FlxGamepad;
 import flixel.input.gamepad.id.XBox360ID;
 import flixel.ui.FlxBar;
@@ -33,26 +35,29 @@ class Player extends FlxSprite
 		this.type = type;
 		this.controlScheme = controlScheme;
 
-		var colour:Int = 0xFF000000;
+		frames = FlxAtlasFrames.fromTexturePackerJson("assets/img/player.png", "assets/img/player.json");
+		animation.addByPrefix("idle", "stand", 0, false);
+		animation.addByPrefix("moveRight", "walkright", 8, false);
+		animation.addByPrefix("moveUp", "walkforward", 8, false);
+		setFacingFlip(FlxObject.LEFT, true, false);
+		setFacingFlip(FlxObject.RIGHT, false, false);
+		setFacingFlip(FlxObject.UP, true, false);
+		setFacingFlip(FlxObject.DOWN, false, false);
+
 		var barFillColour:Int = 0xFF000000;
 
 		if (type == 0) {
-			colour = 0xFFFF0000;
 			barFillColour = 0xFF660000;
 		}
 		if (type == 1) {
-			colour = 0xFF00FF00;
 			barFillColour = 0xFF006600;
 		}
 		if (type == 2)  {
-			colour = 0xFF0000FF;
 			barFillColour = 0xFF000066;
 		}
 		if (type == 3) {
-			colour = 0xFFFF00FF;
 			barFillColour = 0xFF660066;
 		}
-		makeGraphic(16, 16, colour);
 
 		drag.x = maxVelocity.x * 8;
 		drag.y = maxVelocity.y * 8;
@@ -70,53 +75,72 @@ class Player extends FlxSprite
 		var shoot:Bool = false;
 		var speed:Bool = false;
 
-		if (controlScheme == KEYBOARD_0) {
-			if (FlxG.keys.pressed.UP) up = true;
-			if (FlxG.keys.pressed.DOWN) down = true;
-			if (FlxG.keys.pressed.LEFT) left = true;
-			if (FlxG.keys.pressed.RIGHT) right = true;
-			if (FlxG.keys.pressed.COMMA || FlxG.keys.pressed.NUMPADONE) shoot = true;
-			if (FlxG.keys.pressed.PERIOD || FlxG.keys.pressed.NUMPADTWO) speed = true;
-		} else if (controlScheme == KEYBOARD_1) {
-			if (FlxG.keys.pressed.W) up = true;
-			if (FlxG.keys.pressed.S) down = true;
-			if (FlxG.keys.pressed.A) left = true;
-			if (FlxG.keys.pressed.D) right = true;
-			if (FlxG.keys.pressed.F || FlxG.keys.pressed.K) shoot = true;
-			if (FlxG.keys.pressed.G || FlxG.keys.pressed.L) speed = true;
-		} else {
-			var pad:FlxGamepad = null;
+		{ // Update input
+			if (controlScheme == KEYBOARD_0) {
+				if (FlxG.keys.pressed.UP) up = true;
+				if (FlxG.keys.pressed.DOWN) down = true;
+				if (FlxG.keys.pressed.LEFT) left = true;
+				if (FlxG.keys.pressed.RIGHT) right = true;
+				if (FlxG.keys.pressed.COMMA || FlxG.keys.pressed.NUMPADONE) shoot = true;
+				if (FlxG.keys.pressed.PERIOD || FlxG.keys.pressed.NUMPADTWO) speed = true;
+			} else if (controlScheme == KEYBOARD_1) {
+				if (FlxG.keys.pressed.W) up = true;
+				if (FlxG.keys.pressed.S) down = true;
+				if (FlxG.keys.pressed.A) left = true;
+				if (FlxG.keys.pressed.D) right = true;
+				if (FlxG.keys.pressed.F || FlxG.keys.pressed.K) shoot = true;
+				if (FlxG.keys.pressed.G || FlxG.keys.pressed.L) speed = true;
+			} else {
+				var pad:FlxGamepad = null;
 
-			if (controlScheme == CONTROLLER_0) pad = FlxG.gamepads.getByID(0);
-			if (controlScheme == CONTROLLER_1) pad = FlxG.gamepads.getByID(1);
-			if (controlScheme == CONTROLLER_2) pad = FlxG.gamepads.getByID(2);
-			if (controlScheme == CONTROLLER_3) pad = FlxG.gamepads.getByID(3);
+				if (controlScheme == CONTROLLER_0) pad = FlxG.gamepads.getByID(0);
+				if (controlScheme == CONTROLLER_1) pad = FlxG.gamepads.getByID(1);
+				if (controlScheme == CONTROLLER_2) pad = FlxG.gamepads.getByID(2);
+				if (controlScheme == CONTROLLER_3) pad = FlxG.gamepads.getByID(3);
 
-			if (pad != null && pad.connected) {
-				if (pad.getButton(XBox360ID.A).pressed) speed = true;
+				if (pad != null && pad.connected) {
+					if (pad.getButton(XBox360ID.A).pressed) speed = true;
+				}
 			}
 		}
 
-		if (speed && energy >= 5) {
-			energy -= 100/60/4; // total/fps/maxRunTime
-			timeRunning += elapsed;
-		} else timeRunning = 0;
+		{ // Update energy
+			if (speed && energy >= 5) {
+				energy -= 100/60/4; // total/fps/maxRunTime
+				timeRunning += elapsed;
+			} else timeRunning = 0;
 
-		maxVelocity.set(BASE_SPEED, BASE_SPEED);
+			if (energy < 100) energy += ENERGY_GAIN_RATE;
+			if (energy > 100) energy = 100;
+		}
 
-		acceleration.set();
-		if (up) acceleration.y = -maxVelocity.y * 10;
-		if (down) acceleration.y = maxVelocity.y * 10;
-		if (left) acceleration.x = -maxVelocity.x * 10;
-		if (right) acceleration.x = maxVelocity.x * 10;
+		{ // Update movement
+			maxVelocity.set(BASE_SPEED, BASE_SPEED);
 
-		if (energy < 100) energy += ENERGY_GAIN_RATE;
-		if (energy > 100) energy = 100;
+			acceleration.set();
+			if (up) acceleration.y = -maxVelocity.y * 10;
+			if (down) acceleration.y = maxVelocity.y * 10;
+			if (left) acceleration.x = -maxVelocity.x * 10;
+			if (right) acceleration.x = maxVelocity.x * 10;
+		}
+
+		{ // Update animation
+			if (left) facing = FlxObject.LEFT;
+			if (right) facing = FlxObject.RIGHT;
+			if (up) facing = FlxObject.UP;
+			if (down) facing = FlxObject.DOWN;
+
+			if (left || right) animation.play("moveRight")
+			else if (up || down) animation.play("moveUp")
+			else animation.play("idle");
+		}
 
 		super.update(elapsed);
 
-		bar.x = x + width / 2 - bar.width / 2;
-		bar.y = y + height + bar.height / 2;
-		bar.value = energy;
+		{ // Update misc (post update)
+			bar.x = x + width / 2 - bar.width / 2;
+			bar.y = y + height + bar.height / 2;
+			bar.value = energy;
+		}
 	}
 }
