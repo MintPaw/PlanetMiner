0bb2d02de064912b2c99c77938c793bbac235ba0
MintPaw <jerusanders@gmail.com>
2015-09-04 22:49:17 -0600

    Added control recognition

--- src/game/Player.hx
+++ src/game/Player.hx
@@ -1,6 +1,7 @@
 package game;
 
 import flixel.FlxSprite;
+import flixel.FlxG;
 
 class Player extends FlxSprite
 {
@@ -23,5 +24,33 @@ class Player extends FlxSprite
 		if (type == 2) colour = 0xFF0000FF;
 		if (type == 3) colour = 0xFFFF00FF;
 		makeGraphic(16, 16, colour);
+	}
+
+	public override function update(elapsed:Float):Void
+	{
+		var up:Bool = false;
+		var down:Bool = false;
+		var left:Bool = false;
+		var right:Bool = false;
+		var shoot:Bool = false;
+		var speed:Bool = false;
+
+		if (controlScheme == KEYBOARD_0) {
+			if (FlxG.keys.pressed.UP) up = true;
+			if (FlxG.keys.pressed.DOWN) down = true;
+			if (FlxG.keys.pressed.LEFT) left = true;
+			if (FlxG.keys.pressed.RIGHT) right = true;
+			if (FlxG.keys.pressed.COMMA || FlxG.keys.pressed.NUMPADONE) shoot = true;
+			if (FlxG.keys.pressed.PERIOD || FlxG.keys.pressed.NUMPADTWO) speed = true;
+		} else if (controlScheme == KEYBOARD_1) {
+			if (FlxG.keys.pressed.W) up = true;
+			if (FlxG.keys.pressed.S) down = true;
+			if (FlxG.keys.pressed.A) left = true;
+			if (FlxG.keys.pressed.D) right = true;
+			if (FlxG.keys.pressed.F || FlxG.keys.pressed.K) shoot = true;
+			if (FlxG.keys.pressed.G || FlxG.keys.pressed.L) speed = true;
+		}
+
+		super.update(elapsed);
 	}
 }
